beda8a5a27557779a008ace02ea72bb5aa320020
Aaron Cornelius <monsterrmann@hotmail.com>
2026-02-11 08:56:18 -0800

    Refactor

--- Egg Roll Factory/scripts.js
+++ Egg Roll Factory/scripts.js
@@ -1,21 +1,22 @@
-let clicks = 0
+let clicks = 0;
 
 //clicks per second
-let cps = 0
+let cps = 0;
 //clicks per click
-let cpc = 1
+let cpc = 1;
 
 //values for leveling system
-let xp = 0
-let xpNeed = 100
-let level = 1
+let xp = 0;
+let xpNeed = 100;
+let level = 1;
 
 //explosion progress
 let boomprog = 0;
 
 function Load() {
     //check if localstorage has data
-    if (!localStorage.getItem("data")) {
+    let dataRaw = localStorage.getItem("data")
+    if (!dataRaw) {
         //set all values to default
         clicks = 0;
         cps = 0;
@@ -28,7 +29,7 @@ function Load() {
     }
 
     //set all values to saved values
-    let data = JSON.parse(localStorage.getItem("data"));
+    let data = JSON.parse(dataRaw);
     clicks = data[0];
     cps = data[1];
     cpc = data[2];
@@ -67,47 +68,56 @@ function reset() {
     refreshUi();
 }
 
+function levelUp(){
+    if(!xp > xpNeed) return 
+
+    level++;
+    xp -= xpNeed;
+    
+    if(level === 2){
+        xpNeed = 1000;
+    }else{
+        xpNeed *= 1000;
+    }
+
+    refreshUi()
+}
+
+function checkLevelUp() {
+    if (xp >= xpNeed) {
+        levelUp();
+    }
+}
+
 function clicked() {
     //function run when the user clicks
 
     //add clicks and exp
     clicks += cpc;
     xp += cpc;
-    if (xp > xpNeed) {
-        //level up when necesary
-        levelUp();
-    }
+
+    checkLevelUp();
 
     boomprog++;
 
     if (boomprog === 100) boom();
 }
-function levelUp() {
-    if (!xp > xpNeed) return;
-
-    level++;
-    xp -= xpNeed;
-
-    if (level === 2) xpNeed = 1000;
-    else xpNeed *= 1000;
-
-    refreshUi();
-}
 
 function upgrade(resourceCost, increment, type) {
     //available is 1 unless "buy all" is checked
-    let available = document.getElementById("buy_all").checked ? Math.floor(clicks / resourceCost) : 1;
+    let available = document.getElementById("buy_all").checked ? available = Math.floor(clicks / resourceCost) : 1
 
     //double check that the upgrade can be purchased
-    if (!clicks >= resourceCost * available) return;
+    if (!clicks >= resourceCost * available) return
 
     clicks -= resourceCost * available;
 
-    //get upgrade based on type and update html
-    cps += available * increment;
+    if (type === 'cpc') cpc += available * increment;
+    else if (type === 'cps') cps += available * increment;
 
     refreshUi();
 }
+
 function boom() {
     //explosion animation
     boomprog = 0;
@@ -121,10 +131,8 @@ function Run() {
     //increase clicks and xp by cps
     clicks += cps * 0.01;
     xp += cps * 0.01;
-    if (xp > xpNeed) {
-        //level up if necessary
-        levelUp();
-    }
+
+    checkLevelUp();
 
     Save();
     refreshUi();
