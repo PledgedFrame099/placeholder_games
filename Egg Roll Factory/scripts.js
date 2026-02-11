766cc0dac3bd5c0a8e2216ecf5ccd3004d66bb31
Aaron Cornelius <monsterrmann@hotmail.com>
2026-02-11 07:37:07 -0800

    Refactor variable declarations and functions in scripts.js

---
+++ Egg Roll Factory/scripts.js
@@ -0,0 +1,152 @@
+var clicks=0
+
+//clicks per second
+var cps=0
+//clicks per click
+var cpc=1
+
+//values for leveling system
+var xp=0
+var xpNeed=100
+var level=1
+
+//explosion progress
+var boomprog=0
+
+function Load(){
+    //check if localstorage has data
+    if(localStorage.getItem("data")!=null){
+        //set all values to saved values
+        var data=JSON.parse(localStorage.getItem("data"))
+        clicks=data[0]
+        cps=data[1]
+        cpc=data[2]
+        xp=data[3]
+        xpNeed=data[4]
+        level=data[5]
+    }else{
+        //set all values to default
+        clicks=0
+        cps=0
+        cpc=1
+        xp=0
+        xpNeed=100
+        level=1
+    }
+    //ensure ui is up to date
+    refreshUi()
+}
+function Save(){
+    //save all values to localstorage
+    localStorage.setItem("data",JSON.stringify([clicks,cps,cpc,xp,xpNeed,level]))
+}
+function refreshUi(){
+    //update the ui
+    document.getElementById('counter').innerHTML = Math.floor(clicks);
+    document.getElementById('level').innerHTML=level;
+    document.getElementById('to_next_level').innerHTML=Math.floor(xpNeed);
+    document.getElementById('cps').innerHTML = cps;
+    document.getElementById('cpc').innerHTML = cpc;
+    document.getElementById('exp_counter').innerHTML = Math.floor(xp);
+}
+
+function reset(){
+    //ask the user twice before deleting all data and resetting values to default
+    if(confirm("Are you sure you want to reset all of your progress?")&&confirm('Are you really really sure? (You will not recieve anything.)')){
+        localStorage.removeItem("data")
+        clicks=0
+        cps=0
+        cpc=1
+        xp=0
+        xpNeed=100
+        level=1
+        refreshUi()
+    }
+}
+
+function clicked(){
+    //function run when the user clicks
+
+    //add clicks and exp
+    clicks+=cpc
+    xp+=cpc
+    if(xp>xpNeed){
+        //level up when necesary
+        levelUp()
+    }
+
+    boomprog = boomprog + 1;
+    if (boomprog == 100) {
+        boom();
+    }
+}
+function levelUp(){
+    if(xp>xpNeed){
+        //increase level and increase xp required for next level
+        level+=1
+        document.getElementById('level').innerHTML=level
+        xp-=xpNeed
+        if(level==2){
+            xpNeed=1000
+        }else{
+            xpNeed*=1000
+        }
+        document.getElementById('to_next_level').innerHTML=xpNeed
+    }
+}
+
+function upgrade(resourceCost, increment, type) {
+    //available is 1 unless "buy all" is checked
+    var available=1
+    if (document.getElementById("buy_all").checked) {
+        available = Math.floor(clicks/resourceCost);
+    }
+
+    //double check that the upgrade can be purchased
+    if(clicks>=resourceCost*available){
+        //decrease clicks
+        clicks -= resourceCost*available;
+
+        //get upgrade based on type and update html
+        if (type == 'cpc') {
+            cpc += available*increment;
+            document.getElementById('cpc').innerHTML = cpc;
+        }else if (type == 'cps') {
+            cps += available*increment;
+            document.getElementById('cps').innerHTML = cps;
+        }
+    }
+}
+function boom() {
+    //explosion animation
+    boomprog = 0;
+    document.getElementById('image').src="explosion.gif";
+    setTimeout(function(){
+        document.getElementById('image').src="eggroll.png";
+    }, 800);
+}
+
+function Run(){
+    //increase clicks and xp by cps
+    clicks+=cps*0.01
+    xp+=cps*0.01
+    if(xp>xpNeed){
+        //level up if necessary
+        levelUp()
+    }
+
+    Save()
+
+    document.getElementById("counter").innerHTML = Math.floor(clicks);
+    document.getElementById('exp').style.width = (xp/xpNeed)*50 + "%";
+    document.getElementById('exp_counter').innerHTML = Math.floor(xp);
+}
+
+//initialize
+Load()
+refreshUi()
+
+//interval for primary function
+let Inter=setInterval(Run,10)
+//ensure ui is up to date every second
+let ensure=setInterval(refreshUi,1000)
