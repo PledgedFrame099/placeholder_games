8184352a2bcf2a79cb3de940d2d0d3be835d479b
Aaron Cornelius <monsterrmann@hotmail.com>
2026-02-12 09:26:35 -0800

    Reformat with Prettier

--- Egg Roll Factory/scripts.js
+++ Egg Roll Factory/scripts.js
@@ -1,109 +1,117 @@
 let clicks, cps, xp, cpc, level, xpNeed, boomprog;
 
 function setDefaultValues() {
-    [clicks, cps, xp, boomprog] = [0, 0, 0, 0];
-    [cpc, level] = [1, 1];
-    xpNeed = 100;
+  [clicks, cps, xp, boomprog] = [0, 0, 0, 0];
+  [cpc, level] = [1, 1];
+  xpNeed = 100;
 }
 
 setDefaultValues();
 
 function Load() {
-    const dataRaw = localStorage.getItem("data");
-    const data = dataRaw ? JSON.parse(dataRaw) : { clicks: 0, cps: 0, cpc: 1, xp: 0, xpNeed: 100, level: 1 };
+  const dataRaw = localStorage.getItem("data");
+  const data = dataRaw
+    ? JSON.parse(dataRaw)
+    : { clicks: 0, cps: 0, cpc: 1, xp: 0, xpNeed: 100, level: 1 };
 
-    ({ clicks, cps, cpc, xp, xpNeed, level } = data);
-    refreshUi();
+  ({ clicks, cps, cpc, xp, xpNeed, level } = data);
+  refreshUi();
 }
 
 function Save() {
-    localStorage.setItem("data", JSON.stringify({ clicks, cps, cpc, xp, xpNeed, level }));
+  localStorage.setItem(
+    "data",
+    JSON.stringify({ clicks, cps, cpc, xp, xpNeed, level }),
+  );
 }
 
 function getElem(name) {
-    return document.getElementById(name);
+  return document.getElementById(name);
 }
 
 function refreshUi() {
-    getElem('counter').innerHTML = Math.floor(clicks);
-    getElem('level').innerHTML = level;
-    getElem('to_next_level').innerHTML = Math.floor(xpNeed);
-    getElem('cps').innerHTML = cps;
-    getElem('cpc').innerHTML = cpc;
-    getElem('exp').style.width = (xp / xpNeed) * 50 + "%";
-    getElem('exp_counter').innerHTML = Math.floor(xp);
+  getElem("counter").innerHTML = Math.floor(clicks);
+  getElem("level").innerHTML = level;
+  getElem("to_next_level").innerHTML = Math.floor(xpNeed);
+  getElem("cps").innerHTML = cps;
+  getElem("cpc").innerHTML = cpc;
+  getElem("exp").style.width = (xp / xpNeed) * 50 + "%";
+  getElem("exp_counter").innerHTML = Math.floor(xp);
 }
 
 function reset() {
-    if (!confirm("Are you sure you want to reset all of your progress?")) return;
-    if (!confirm('Are you really really sure? (You will not recieve anything.)')) return;
+  if (!confirm("Are you sure you want to reset all of your progress?")) return;
+  if (!confirm("Are you really really sure? (You will not recieve anything.)"))
+    return;
 
-    localStorage.removeItem("data");
-    Load();
+  localStorage.removeItem("data");
+  Load();
 }
 
 function levelUp() {
-    level++;
+  level++;
 
-    xp -= xpNeed;
+  xp -= xpNeed;
 
-    xpNeed = level === 2 ? 1000 : xpNeed * 1000;
+  xpNeed = level === 2 ? 1000 : xpNeed * 1000;
 
-    refreshUi();
+  refreshUi();
 }
 
 function checkLevelUp() {
-    while (xp >= xpNeed) {
-        levelUp();
-    }
+  while (xp >= xpNeed) {
+    levelUp();
+  }
 }
 
 function clicked() {
-    clicks += cpc;
-    xp += cpc;
+  clicks += cpc;
+  xp += cpc;
 
-    checkLevelUp();
+  checkLevelUp();
 
-    boomprog++;
-    if (boomprog === 100) boom();
+  boomprog++;
+  if (boomprog === 100) boom();
 }
 
 function upgrade(resourceCost, increment, type) {
-    //available is 1 unless "buy all" is checked
-    let available = getElem("buy_all").checked ? Math.floor(clicks / resourceCost) : 1;
+  //available is 1 unless "buy all" is checked
+  let available = getElem("buy_all").checked
+    ? Math.floor(clicks / resourceCost)
+    : 1;
 
-    const totalCost = resourceCost * available;
+  const totalCost = resourceCost * available;
 
-    if (clicks < totalCost) return;
+  if (clicks < totalCost) return;
 
-    clicks -= totalCost;
+  clicks -= totalCost;
 
-    const amountToAdd = available * increment;
+  const amountToAdd = available * increment;
 
-    if (type === 'cpc') cpc += amountToAdd;
-    else if (type === 'cps') cps += amountToAdd;
+  if (type === "cpc") cpc += amountToAdd;
+  else if (type === "cps") cps += amountToAdd;
 
-    refreshUi();
+  refreshUi();
 }
 
 //explosion animation
 function boom() {
-    boomprog = 0;
-    getElem('image').src = "explosion.gif";
-    setTimeout(() => {
-        getElem('image').src = "eggroll.png";
-    }, 800);
+  boomprog = 0;
+  getElem("image").src = "explosion.gif";
+  setTimeout(() => {
+    getElem("image").src = "eggroll.png";
+  }, 800);
 }
 
 setInterval(() => {
-    const gain = cps / 100;
-    clicks += gain;
-    xp += gain;
+  const gain = cps / 100;
+  clicks += gain;
+  xp += gain;
 
-    checkLevelUp();
+  checkLevelUp();
 
-    Save();
-    refreshUi();
+  Save();
+  refreshUi();
 }, 10);
 
 Load();
